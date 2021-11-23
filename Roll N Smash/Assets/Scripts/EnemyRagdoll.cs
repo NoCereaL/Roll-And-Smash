ad69304eaf152dfd0b524230997b4f3899d5cf47
NoCereaL <therealskm@hotmail.com>
2021-11-23 02:46:09 +0000

    Ragdoll Update

--- Roll N Smash/Assets/Scripts/EnemyRagdoll.cs
+++ Roll N Smash/Assets/Scripts/EnemyRagdoll.cs
@@ -6,21 +6,33 @@ public class EnemyRagdoll : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
     [SerializeField] Rigidbody[] rigidbodies;
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] Collider collider;
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckEnemyIsDead();
+    }
+
+    public void CheckEnemyIsDead()
+    {
+        if(enemy.GetComponent<EnemyScript>().dead == true)
+        {
+            rigidbody.isKinematic = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "Player")
         {
+            enemy.GetComponent<EnemyScript>().dead = true;
             for (int i = 0; i >= rigidbodies.Length; i--)
             {
                 rigidbodies[i].isKinematic = false;
